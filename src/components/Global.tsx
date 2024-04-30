12f09a5abacaf8f03aa5065c8bcdc63ccdf935c4
pAx24 <kalestrov.mo@gmail.com>
2024-04-30 18:11:00 +0300

    "Forgot the rest"

--- src/components/Global.tsx
+++ src/components/Global.tsx
@@ -3,7 +3,13 @@ import '../css/Global.css';
 
 export class Header extends Component<any, any> {
     render(){
-        console.log(this.props.chessTitle);
+        if(this.props.chessTitle)   return (<img className='Header'src='/media/logo.png'/>);
+        else                        return (<h1 className='Header'>Botev</h1>)
+    };
+}
+
+export class Lazy extends Component<any, any> {
+    render(){
         if(this.props.chessTitle)   return (<img className='Header'src='/media/logo.png'/>);
         else                        return (<h1 className='Header'>Botev</h1>)
     };
